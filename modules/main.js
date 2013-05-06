8ee44a369175faad165021a238d3769689f3285d
Shengzhou Zhang <steven.shengzhou@gmail.com>
2013-05-06 13:42:02 +1000

    Update main.js

--- modules/main.js
+++ modules/main.js
@@ -16,14 +16,14 @@ function($, IRT) {
 	irt.addWord("raca", 2);
 	irt.end("");
 	
-	var S2 = "$This$is$the$first$demo$.";
-	var irt2 = new IRT(S2);
+//	var S2 = "$This$is$the$first$demo$.";
+//	var irt2 = new IRT(S2);
 	
-	irt2.removeWord("first");
-	irt2.addWord("second", 4);
-	irt2.end("");
+//	irt2.removeWord("first");
+//	irt2.addWord("second", 4);
+//	irt2.end("");
 	
-	var S3 = "$This$is$the$second$demo$.";
-	var irt3 = new IRT(S3);
-	irt3.end("Should be the same as the previous one after add word \"second\".");
+//	var S3 = "$This$is$the$second$demo$.";
+//	var irt3 = new IRT(S3);
+//	irt3.end("Should be the same as the previous one after add word \"second\".");
 });
